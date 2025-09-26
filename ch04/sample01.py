ec3cc4730ffb14c921cfd495218343ab63a91b74
parkj00won <88juwon@naver.com>
2025-09-26 10:31:40 +0900

    Update files with Git LFS

--- ch04/sample01.py
+++ ch04/sample01.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
 covid_file_path = './data/owid-covid-data.csv'
-df = pd.read_csv(covid_file_path)
+covid_file_path = './data/data_bar.csv'
+df = pd.read_csv(covid_file_path, sep='|')
 print(df)
