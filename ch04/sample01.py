5e136fbcf6a7da206f6679c63e599d50f732586a
parkj00won <88juwon@naver.com>
2025-09-26 11:39:16 +0900

    4차시 끝

--- ch04/sample01.py
+++ ch04/sample01.py
@@ -1,3 +1,5 @@
+from importlib.util import source_hash
+
 import pandas as pd
 
 covid_file_path = './data/owid-covid-data.csv'
@@ -18,3 +20,23 @@ df.info()
 
 print('-'*50)
 print(df.head())
+
+#원하는 컬럼만
+selected_columns = ['iso_code', 'location', 'date','total_cases','population']
+selected_df = df[selected_columns]
+print('-'*50)
+print(selected_df.head())
+
+#locations = df['loaction']
+locations = selected_df['location']
+print('-'*50)
+print(locations)
+
+print('-'*50)
+print(locations.unique())
+print(locations.unique().size)
+
+#South Korea
+south_korea_df = selected_df[selected_df['iso_code'] == 'South Korea']
+print('-'*50)
+print(south_korea_df.head())
