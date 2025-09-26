0eb225798e381d4705d5ddb25d5348eae0ada671
parkj00won <88juwon@naver.com>
2025-09-26 11:23:37 +0900

    4차시 테스트

--- ch04/sample01.py
+++ ch04/sample01.py
@@ -1,6 +1,20 @@
 import pandas as pd
 
 covid_file_path = './data/owid-covid-data.csv'
-covid_file_path = './data/data_bar.csv'
-df = pd.read_csv(covid_file_path, sep='|')
-print(df)
+#ver1
+#covid_file_path = './data/data_bar.csv'
+#df = pd.read_csv(covid_file_path, sep='|')
+#print(df)
+
+#ver2
+# covid_file_path = './data/data_bar_euckr.csv'
+# df = pd.read_csv(covid_file_path, sep='|', encoding='euc-kr') #읽기 가능
+#print(df)
+
+df = pd.read_csv(covid_file_path)
+
+print('-'*50)
+df.info()
+
+print('-'*50)
+print(df.head())
